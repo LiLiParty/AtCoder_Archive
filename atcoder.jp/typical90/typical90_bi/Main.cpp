5328aaa2037bbe3fe1e7240ab613885da7711456
RiRiPa <okadango33@gmail.com>
2021-09-15 17:21:36 +0900

    [AC] typical90 typical90_bi

---
+++ atcoder.jp/typical90/typical90_bi/Main.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define pp pair<ll,ll>
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define INF32 2147483647 
+#define INF64 9223372036854775807
+#define rep(i, n) for(int i = 0; i < n; i++)
+const ll MOD = 1000000007;//10^9+7
+const ll mod = 998244353;
+
+int main() {
+    vector<int> ans;
+    int Q; cin >> Q;
+    deque<int> dq;
+    for (int i = 0; i < Q; i++){
+        int t,x; cin >> t >> x;
+        if(t == 1){
+            dq.push_front(x);
+        }else if(t == 2){
+            dq.push_back(x);
+        }else{
+            ans.push_back(dq.at(x-1));
+        }
+    }
+    for(int a: ans){
+        cout << a << endl;
+    }
+}
