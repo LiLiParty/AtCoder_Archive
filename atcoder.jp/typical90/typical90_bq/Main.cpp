c5b92c8788ef02faed7aaad5d3d22ee4930202e6
RiRiPa <okadango33@gmail.com>
2021-09-15 21:29:17 +0900

    [AC] typical90 typical90_bq

---
+++ atcoder.jp/typical90/typical90_bq/Main.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define pp pair<ll,ll>
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define INF32 2147483647 
+#define INF64 9223372036854775807
+#define rep(i, n) for(int i = 0; i < n; i++)
+const ll MOD = 1000000007;//10^9+7
+const ll mod = 998244353;
+
+//a^n mod(p)
+ll modPow(long long a, long long n,  long p) {
+    if (n == 0) return 1; // 0乗にも対応する場合
+    if (n == 1) return a % p;
+    if (n % 2 == 1) return (a * modPow(a, n - 1, p)) % p;
+    long long t = modPow(a, n / 2, p);
+    return (t * t) % p;
+}
+
+int main() {
+	ll N,K; cin >> N >> K;
+    ll ans;
+    if(N == 1){
+        cout << K << endl;
+    }else if(N == 2){
+        if(K == 1){
+            cout << 0 << endl;
+        }else{
+            ans = K * (K - 1);
+            ans %= MOD;
+            cout << ans << endl;
+        }
+    }else{
+        if(K == 1 || K == 2){
+            cout << 0 << endl;
+        }else{
+            ans = K * (K - 1);
+            ans %= MOD;
+            ans *= modPow(K-2,N-2,MOD);
+            ans %= MOD;
+            cout << ans << endl;
+        }
+    }
+}
