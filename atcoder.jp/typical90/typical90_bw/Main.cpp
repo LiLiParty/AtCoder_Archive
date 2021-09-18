08ad0c3ba8583d9a3e6d2fdf451953cc710c35c3
RiRiPa <okadango33@gmail.com>
2021-09-18 14:20:03 +0900

    [AC] typical90 typical90_bw

---
+++ atcoder.jp/typical90/typical90_bw/Main.cpp
@@ -0,0 +1,105 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define pp pair<ll,ll>
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define INF32 2147483647 
+#define INF64 9223372036854775807
+#define rep(i, n) for(int i = 0; i < n; i++)
+const ll MOD = 1000000007;//10^9+7
+const ll mod = 998244353;
+
+//素因数分解 ex:{{2, 2}, {5, 1}, {101, 1}}
+vector<pair<long long, long long> > prime_factorize(long long N) {
+    vector<pair<long long, long long> > res;
+    for (long long a = 2; a * a <= N; ++a) {
+        if (N % a != 0) continue;
+        long long ex = 0; // 指数
+        // 割れる限り割り続ける
+        while (N % a == 0) {
+            ++ex;
+            N /= a;
+        }
+        // その結果を push
+        res.push_back(mk(a, ex));
+    }
+    // 最後に残った数について
+    if (N != 1) res.push_back(mk(N, 1));
+    return res;
+}
+
+int main() {
+    ll N;   
+    cin >> N;
+    vector<pp> pf = prime_factorize(N);
+
+    vector<ll> G;
+    ll two = 1;
+    for(int i=0; i<100; i++){
+        G.push_back(two - 1);
+        two *= 2;
+        if(G[i] == 9223372036854775807LL) break;
+        //cout << G[i] << endl;
+    }
+
+    ll count = -1;
+    for(pp p: pf){
+        count += p.second;
+    }
+    //cout << count << endl;
+    auto itr = lower_bound(G.begin(),G.end(),count);
+    ll ans = distance(G.begin(),itr);
+    cout << ans << endl;
+    //cout << ans << endl;
+}
