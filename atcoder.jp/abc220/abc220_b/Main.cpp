83236e904c043945acbf7ae653a1da155214613d
RiRiPa <okadango33@gmail.com>
2021-09-26 21:08:40 +0900

    [AC] abc220 abc220_b

---
+++ atcoder.jp/abc220/abc220_b/Main.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define pp pair<ll,ll>
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define INF32 2147483647 
+#define INF64 9223372036854775807
+#define rep(i, n) for(int i = 0; i < n; i++)
+const ll MOD = 1000000007;//10^9+7
+const ll mod = 998244353;
+
+ll henkan(ll N,ll k){
+    ll num = 1;
+    ll ans = 0;
+    while(N!=0){
+        ans += (N%10)*num;
+        N/=10;
+        num*=k;
+    }
+    return ans;
+}
+
+int main() {
+    ll K,A,B; cin >> K >> A >> B;
+    A = henkan(A,K);
+    B = henkan(B,K);
+    ll ans = A*B;
+    cout << ans << endl;
+}
