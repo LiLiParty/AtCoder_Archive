55295b995e62b6c31201baf52d2c13048d989259
RiRiPa <okadango33@gmail.com>
2021-10-02 21:20:19 +0900

    [AC] abc221 abc221_b

---
+++ atcoder.jp/abc221/abc221_b/Main.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define pp pair<ll,ll>
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define INF32 2147483647 
+#define INF64 9223372036854775807
+#define rep(i, n) for(int i = 0; i < n; i++)
+const ll MOD = 1000000007;//10^9+7
+const ll mod = 998244353;
+
+
+int main() {
+    string S,T; cin >> S >> T;
+    if(S == T){
+        cout << "Yes" << endl;
+        return 0;
+    }
+    int N = S.size();
+    for(int i=0; i<N-1; i++){
+        string Sc = S;
+        Sc[i+1] = S[i];
+        Sc[i] = S[i+1];
+        if(Sc == T){
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
