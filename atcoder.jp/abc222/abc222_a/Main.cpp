3bed3d2a4b02c86e7d719f6d61828624a962d938
RiRiPa <okadango33@gmail.com>
2021-10-16 22:53:51 +0900

    [AC] abc222 abc222_a

---
+++ atcoder.jp/abc222/abc222_a/Main.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+#define pp pair<ll,ll>
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define INF32 2147483647 
+#define INF64 9223372036854775807
+#define rep(i, n) for(int i = 0; i < n; i++)
+const ll MOD = 1000000007;//10^9+7
+const ll mod = 998244353;
+
+int main(){
+    string N; cin >> N;
+    string a;
+    for(int i=0; i<4-N.size(); i++){
+        a.push_back('0');
+    }
+    cout << a << N << endl;
+}
