dd0b27f3b18be4c786c05910d5be8d9d6fa589b2
RiRiPa <okadango33@gmail.com>
2023-05-07 11:43:07 +0900

    [AC] abc229 abc229_a

---
+++ atcoder.jp/abc229/abc229_a/Main.cpp
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+using namespace std;
+#define pp pair<ll,ll>
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define INF32 2147483647 
+#define INF64 9223372036854775807
+#define rep(i, n) for(int i = 0; i < n; i++)
+const ll MOD = 1000000007;//10^9+7
+//const ll mod = 998244353;
+
+
+
+int main() {
+    string S1,S2;
+    cin >> S1 >> S2;
+    
+    int count = 0;
+    if(S1[0] == '#') count++;
+    if(S1[1] == '#') count++;
+    if(S2[0] == '#') count++;
+    if(S2[1] == '#') count++;
+
+    if(count==2 and S1[0] == '#' and S2[1] == '#'){
+        cout << "No" << endl;
+    }else if(count==2 and S1[1] == '#' and S2[0] == '#'){
+        cout << "No" << endl;
+    }else{
+        cout << "Yes" << endl;
+    }
+}
