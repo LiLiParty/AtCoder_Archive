4579747a261a9675d15bd62f973becd4a8cc2bbe
RiRiPa <okadango33@gmail.com>
2023-05-17 11:23:42 +0900

    [AC] abc232 abc232_b

---
+++ atcoder.jp/abc232/abc232_b/Main.cpp
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <cassert>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mk make_pair
+#define INF32 2147483647 
+#define INF64 9223372036854775807
+const ll MOD = 1000000007;//10^9+7
+//const ll mod = 998244353;
+
+ll get_digit(ll num){
+    ll digit = 0;
+    while(num != 0){
+        num /= 10;
+        digit++;
+    }
+    
+    return digit;
+}
+
+
+
+int main() {
+    string S,T;
+    cin >> S >> T;
+    
+    int n1 = S[0] - T[0];
+    if(n1<0) n1+=26;
+    for(int i=1; i<S.length(); i++){
+        int n = S[i] - T[i];
+        if(n<0) n+=26; 
+        if(n1 != n){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    
+    cout << "Yes" << endl;
+}
